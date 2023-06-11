ddb92ed03d6a19be322ac873453f94b88d47a465
Ranvir Singh <124471296+heyravnvir@users.noreply.github.com>
2023-06-11 00:36:56 +0000

    interfaced commands changed

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -1,7 +1,52 @@
 ﻿namespace Assignment.InterfaceCommand;
 
-interface RobotCommand
+public interface RobotCommand
 {
     void Run(Robot robot); // Interfaces are public and abstract by default.
 }
+public class OffCommand : RobotCommand{
+    public void Run(Robot robot)=> robot.IsPowered=false;}
 
+public class OnCommand : RobotCommand{
+    public void Run(Robot robot) =>robot.IsPowered=true;}
+
+public class WestCommand : RobotCommand{
+    public void Run(Robot robot) { 
+        if (robot.IsPowered){
+            robot.X--;
+            } 
+        }
+}
+
+public class EastCommand : RobotCommand{
+    public void Run(Robot robot) { 
+        if (robot.IsPowered) {
+            robot.X++;}
+            
+            }
+}
+
+public class SouthCommand : RobotCommand{
+    public void Run(Robot robot) {
+         if (robot.IsPowered){
+            robot.Y--;
+         }
+            }
+}
+
+public class NorthCommand : RobotCommand{
+    public void Run(Robot robot) { 
+        if (robot.IsPowered){
+            robot.Y++;}
+             }
+}
+
+public class RebootCommand : RobotCommand{
+    public void Run(Robot robot) 
+    { if (robot.IsPowered) {
+        robot.Y = 0; 
+        robot.X = 0; 
+        robot.IsPowered = false; 
+        }
+    }
+}
